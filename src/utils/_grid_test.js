2a601ef2e7bedd6636ae8d9aef761c0bc9b7bac9
James Salt <saltpy@gmail.com>
2014-01-28 17:20:08 -0500

    Issue: should be able to place and retrieve pieces on grid by location

--- src/utils/_grid_test.js
+++ src/utils/_grid_test.js
@@ -2,10 +2,24 @@
 
 var assert = require("better-assert");
 var Grid = require("./grid");
+var Location = require("./location");
+var Piece = require("./piece");
 
 function testBlankGrid() {
   var g = Grid.blank();
   assert(null === g.pieces);
 }
 
+function testPiecePlacement() {
+  var g = Grid.blank();
+  var testPiece = Piece.construct("test");
+  var topLeft = Location.construct(0, 0);
+
+  g.place(testPiece, topLeft);
+
+  assert(testPiece === g.contentsOfLocation(topLeft));
+  assert(null !== g.pieces);
+}
+
 testBlankGrid();
+testPiecePlacement();
