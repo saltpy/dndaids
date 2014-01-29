22b8feafbae36ce63dd220f5fdf571f5fdf6c6e5
James Salt <saltpy@gmail.com>
2014-01-29 09:08:41 -0500

    Fixed: 4cabc8b

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -1,7 +1,12 @@
 "use strict";
 
-exports.construct = function() {
-  return {};
+exports.construct = function(x, y) {
+  if (typeof x === undefined) x = 0;
+  if (typeof y === undefined) y = 0;
+  var struct = {};
+  struct.x = x;
+  struct.y = y;
+  return struct;
 };
 
 exports.place = function(grid, piece, loc) {
