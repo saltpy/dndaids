b61be79c2b7a617ccb069ef849608619050873d8
James Salt <saltpy@gmail.com>
2014-01-29 09:20:54 -0500

    Added: expose raw assert from the facade

--- src/utils/test_utils.js
+++ src/utils/test_utils.js
@@ -3,17 +3,19 @@
 var assert = require("better-assert");
 
 exports.assertSameType = function(a, b) {
-  assert(Object.prototype.toString.call(a) === Object.prototype.toString.call(b));
+  exports.assert(Object.prototype.toString.call(a) === Object.prototype.toString.call(b));
 };
 
 exports.assertArrayEqual = function(a, b) {
   exports.assertSameType(a, b);
-  assert(a.length === b.length);
+  exports.assert(a.length === b.length);
   for (var i = 0; i < a.length; i++) {
-    assert(a[i] === b[i]);
+    exports.assert(a[i] === b[i]);
   }
 };
 
 exports.assertStructEqual = function(a, b) {
   exports.assertArrayEqual(Object.keys(a).sort(), Object.keys(b).sort());
 };
+
+exports.assert = assert;
